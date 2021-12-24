bf91dd6eec0579bb43426be0b4d80f515465ca47
Paul Holden <paul@paulholden.dev>
2021-12-24 10:36:16 -0600

    Load .env for dev/test environments

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -1,4 +1,7 @@
 require "active_support/core_ext/integer/time"
+require "dotenv"
+
+Dotenv.load
 
 Rails.application.configure do
   # Settings specified here will take precedence over those in config/application.rb.
