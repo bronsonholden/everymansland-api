3759f3c5579d9d1ce3f692bf19c8902a6b7d762d
Paul Holden <paul@paulholden.dev>
2021-12-23 22:22:03 -0600

    Enable cookies middleware

--- config/application.rb
+++ config/application.rb
@@ -31,5 +31,7 @@ module EveryMansLand
     # Middleware like session, flash, cookies can be added back manually.
     # Skip views, helpers and assets when generating a new resource.
     config.api_only = true
+
+    config.middleware.use ActionDispatch::Cookies
   end
 end
