f032ec309aa3cae386f729a9bf10e849926d856d
Paul Holden <paul@paulholden.dev>
2021-12-30 19:40:55 -0600

    Add some ForbiddenError helpers

--- app/errors/forbidden_error.rb
+++ app/errors/forbidden_error.rb
@@ -2,4 +2,16 @@ class ForbiddenError < ApplicationError
   def self.no_permission(to_do)
     new("You do not have permission to #{to_do}")
   end
+
+  def self.no_show_permission(record)
+    self.no_permission("view this #{record.class.name.downcase}")
+  end
+
+  def self.no_update_permission(record)
+    self.no_permission("change this #{record.class.name.downcase}")
+  end
+
+  def self.no_destroy_permission(record)
+    self.no_permission("delete this #{record.class.name.downcase}")
+  end
 end
